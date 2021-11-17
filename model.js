a18693e64f91689f5eec103ed1adfe7cd8a102dc
manfredmichael <koskosanrider@gmail.com>
2021-11-17 16:08:27 +0700

    initial commit

--- model.js
+++ model.js
@@ -1,21 +1,32 @@
-const tf = require('@tensorflow/tfjs-node');
-const movies = require('./data/movielens100k_details.json');
+const tf = require("@tensorflow/tfjs-node");
+// const tf = require("@tensorflow/tfjs");
+const movies = require("./data/movielens100k_details.json");
+
+let isLoadingModel = false;
+let modelLoaded = false;
+let model;
 
 function loadData() {
   const movie_arr = [];
   for (let i = 0; i < movies.length; i++) {
-    movie_arr.push([movies[i]['movie_id']]);
+    movie_arr.push([movies[i]["movie_id"]]);
   }
   return movie_arr;
 }
 
 async function loadModel() {
-  console.log('Loading Model...');
-  model = await tf.loadLayersModel(
-    `file://${__dirname}/model/model.json`,
+  console.log("Loading Model...");
+  // const model = await tf.loadLayersModel(
+  //   "https://raw.githubusercontent.com/hafezdeldaffa/week3-ml-movielens/master/model/model.json",
+  //   false
+  // );
+  const model = await tf.loadLayersModel(
+    "file:///home/manfred/Projects/week3-ml-movielens/model/model.json",
     false
   );
-  console.log('Model Loaded Successfull');
+  console.log("Model Loaded Successfull");
+  modelLoaded = true;
+  return model;
   // model.summary()
 }
 
@@ -23,21 +34,30 @@ const movie_arr = tf.tensor(loadData());
 const movie_len = movies.length;
 
 exports.recommend = async function recommend(userId) {
+  console.log(modelLoaded);
   let user = tf.fill([movie_len], Number(userId));
   let movie_in_js_array = movie_arr.arraySync();
-  await loadModel();
-  console.log(`Recommending for User: ${userId}`);
-  pred_tensor = await model.predict([movie_arr, user]).reshape([movie_len]);
-  pred = pred_tensor.arraySync();
-
   let recommendations = [];
-  for (let i = 0; i < 6; i++) {
-    max = pred_tensor.argMax().arraySync();
-    recommendations.push(movies[max]); //Push movie with highest prediction probability
-    pred.splice(max, 1); //drop from array
-    pred_tensor = tf.tensor(pred); //create a new tensor
-  }
+  if (!modelLoaded) {
+    if (!isLoadingModel) {
+      isLoadingModel = true;
+      model = await loadModel();
+    }
+    for (let i = 0; i < 6; i++) {
+      recommendations.push(movies[i]); //Push movie with highest prediction probability
+    }
+  } else {
+    console.log(`Recommending for User: ${userId}`);
+    pred_tensor = await model.predict([movie_arr, user]).reshape([movie_len]);
+    pred = pred_tensor.arraySync();
 
-  console.log(recommendations);
+    for (let i = 0; i < 6; i++) {
+      max = pred_tensor.argMax().arraySync();
+      recommendations.push(movies[max]); //Push movie with highest prediction probability
+      pred.splice(max, 1); //drop from array
+      pred_tensor = tf.tensor(pred); //create a new tensor
+    }
+  }
+  console.log("returned");
   return recommendations;
 };
